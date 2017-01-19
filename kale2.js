e33558ee808cf439b8512725ad776db8639cebe8
Constance Ye <constanceye1316@gmail.com>
2017-01-18 23:28:20 -0800

    made it cleaner
    not to be vain.. but the images it generates are so beautiful i want to
    cry when i look at them thank you p5js

--- kale2.js
+++ kale2.js
@@ -6,17 +6,17 @@ function setup() {
     triheight = (triwidth / 2) * Math.sqrt(3);
     can = createCanvas(windowWidth, windowHeight);
     translate(200, 200);
-    pg.fill(randomColor(0, 255, 20));
+    pg.fill(randomColor(20));
     pg.noStroke();
     pg.triangle(0, 0, triwidth/2, triheight, triwidth, 0);
     pg.blendMode(LIGHTEST);
 
     for(var i = 0; i < 12; i++){
     pg.strokeWeight(30);
-    pg.stroke(randomColor1());
+    pg.stroke(randomColor(Math.random()*255));
     pg.line(Math.random()* triwidth, Math.random()* triheight, Math.random()* triwidth, Math.random()* triheight);}
     for(var i = 0; i < 12; i++){
-    var temp = randomColor1();
+    var temp = randomColor(Math.random()*255);
     pg.noStroke();
     pg.fill(temp);
     pg.ellipse(Math.random()* triwidth,Math.random()* triheight,Math.random()* triwidth/1.5,Math.random()* triheight/1.5);}
@@ -38,36 +38,18 @@ function setup() {
         scale(-1, 1);
          rotate(radians(60));
         image(pg, -100, -(100) * Math.sqrt(3));
-
-    
-
 }
 
-
-
-
-
-
-
-
-function randomColor1() {
+function randomColor(opacity) {
     var colorar = [];
     colorar.push(255);
     for (var i = 0; i < 2; i++) {
         var number = Math.random()*255;
         colorar.push(number);}
     colorar = shuffleArray(colorar);
-    return color(colorar[0], colorar[1], colorar[2], Math.random()*200 + 50);}
-
-function randomColor(lower, upper, opacity) {
-    var colorar = [];
-    colorar.push(upper);
-    for (var i = 0; i < 2; i++) {
-        var number = Math.random() * lower;
-        colorar.push(number);}
-    colorar = shuffleArray(colorar);
     return color(colorar[0], colorar[1], colorar[2], opacity);}
 
+//notmine, fischer yates shuffle
 function shuffleArray(array) {
     for (var i = array.length - 1; i > 0; i--) {
         var j = Math.floor(Math.random() * (i + 1));
